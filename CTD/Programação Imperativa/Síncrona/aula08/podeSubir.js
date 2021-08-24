55da9eab5d72f036ecb36d6bd5e8a3f991bb8971
Gladston de Paula <gladston.dev@gmail.com>
2021-08-24 11:29:29 -0300

    Refatoração função podeSubir

--- CTD/Programação Imperativa/Síncrona/aula08/podeSubir.js
+++ CTD/Programação Imperativa/Síncrona/aula08/podeSubir.js
@@ -1,20 +1,32 @@
+var yesOrNot = false;
+var acomp = false;
+var altura = "";
 
 function podeSubir(height, accompanied) {
-    let podeSubir = false;
-    let accomp = false;
-    
     if (height <= 2 && height > 1.40) {
-        podeSubir = true;
-        console.log("[OK] Can go Up!!!")
-    } else if ((height <= 1.40 && height >= 1.20) && accompanied) {
-        podeSubir = true;
-        accomp = true;
-        console.log("[ATTENTION] Can only go Up accompanied.")
+        yesOrNot = true;
+        altura = height;
+    } else if (height <= 1.4 && height >= 1.2 && accompanied) {
+        yesOrNot = true;
+        acomp = true;
+        altura = height;
     } else {
-        podeSubir = false;
-        console.log("[DENIED] Can't go Up.")
+        yesOrNot = false;
     }
-    return podeSubir
+    return
 }
 
-podeSubir()
+podeSubir(1.35, true);
+
+if (yesOrNot === true && altura > 1.4) {
+    console.log("[OK] Can go Up!!!")
+} else if (altura <= 1.4 && altura >= 1.2 && acomp === true) {
+    console.log("[ATTENTION] Can only go Up accompanied.")
+} else {
+    console.log("[DENIED] Can't go Up.")
+}
+
+
+console.log(yesOrNot);
+console.log(acomp);
+console.log(altura)
