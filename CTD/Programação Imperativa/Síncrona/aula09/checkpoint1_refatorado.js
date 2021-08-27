f362e17e4b894c4062d71560472a6b55c0e4f1c3
Gladston de Paula <gladston.dev@gmail.com>
2021-08-27 11:57:29 -0300

    Removido laço for que contava segundos. Se o número fosse muito grande, iria travar o terminal.

--- CTD/Programação Imperativa/Síncrona/aula09/checkpoint1_refatorado.js
+++ CTD/Programação Imperativa/Síncrona/aula09/checkpoint1_refatorado.js
@@ -1,17 +1,3 @@
-/* - Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
-
-      1 - Pipoca – 10 segundos (padrão);
-      2 - Macarrão – 8 segundos (padrão);
-      3 - Carne – 15 segundos (padrão);
-      4 - Feijão – 12 segundos (padrão);
-      5 - Brigadeiro – 8 segundos (padrão); 
-
-- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
-- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
-- Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!". */
-
 
 // Informações para o usuário.
 console.log("####################################");
@@ -71,47 +57,28 @@ function microondas(opcao, tempo) {
 }
 
 //Chamando a funçao e passando os parâmetros
-microondas(4, 8)
+microondas(1, 30)
 
 //Condicionais caso o usuário determine um tempo diferente do padrão
 if (tmp >= result * 2 && tmp < result * 3) {
     console.log(`Você escolheu ${opcUser}.`);
-            console.log(`Vai levar apenas ${pipoca}seg.`);
+    console.log(`Vai levar apenas ${pipoca}seg.`);
 
-            // Laço de repetição para contar os segundos.
-            for (let i = 1; i <= tmp; i++) {
-                console.log(`${i}..`);
-
-            }
-            console.log("Queimou!");
+    console.log("Queimou!");
 } else if (tmp < result) {
     console.log(`Você escolheu ${opcUser}.`);
     console.log(`Vai levar apenas ${tmp}seg.`);
 
-    let i = tmp;
-    for (let i = 1; i < tmp; i++) {
-        console.log(`${i}..`);
-
-    }
     console.log("Tempo Insuficiente.");
 } else if (tmp >= result * 3) {
     console.log(`Você escolheu ${opcUser}.`);
     console.log(`Vai levar apenas ${tmp}seg.`);
 
-    let i = tmp;
-    for (let i = 1; i < tmp; i++) {
-        console.log(`${i}..`);
-
-    }
     console.log("Kabuuuum!!!");
 } else {
     console.log(`Você escolheu ${opcUser}.`)
     console.log(`Vai levar apenas ${result}seg.`)
 
-    for (let i = 1; i < tmp; i++) {
-        console.log(`${i}..`)
-
-    }
     console.log("Prato pronto, bom apetite!!!")
 }
 
