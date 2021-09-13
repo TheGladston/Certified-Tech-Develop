7d2ce059535edbd67f63d1bcd7b5a1a03f4adf33
Gladston de Paula <gladston.dev@gmail.com>
2021-09-13 14:13:58 -0300

    Aula 13 JSON Programação imperativa

---
+++ CTD/Programação Imperativa/Síncrona/aula13/ex02Json.js
@@ -0,0 +1,13 @@
+// Convertendo objeto JSON em JS (PARSE)
+// Convertendo objeto JS em JSON (STRINGIFY)
+
+let frutas = '{"Fruta":"Banana", "Preço":8.50,"Tipo":"nanica", "Origem":"Brasil"}';
+console.log(frutas);
+
+//Convertendo para JS
+let frutasConvertidasJS = JSON.parse(frutas);
+console.log(frutasConvertidasJS);
+
+//convertendo para JSON
+let frutasConvertidasJSON = JSON.stringify(frutasConvertidasJS);
+console.log(frutasConvertidasJSON);
