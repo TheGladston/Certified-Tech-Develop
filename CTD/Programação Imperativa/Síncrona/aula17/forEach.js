e0a26d58eeb3a61dfa00d73185d978c7b3c0a895
Gladston de Paula <gladston.dev@gmail.com>
2021-09-20 18:01:41 -0300

    Nova solução

--- CTD/Programação Imperativa/Síncrona/aula17/forEach.js
+++ CTD/Programação Imperativa/Síncrona/aula17/forEach.js
@@ -4,7 +4,8 @@ forEach() é um método que chama uma função para cada elemento.
 
 let pizza = ['fatia1', 'fatia2', 'fatia3', 'fatia4'];
 let pessoas = ['Gladston', 'Val', 'Lis', 'Célia'];
-let pratos = [];
+let prato = [];
+let count = 0;
 
 pessoas.forEach(
     function (pessoa, i) {
@@ -12,3 +13,14 @@ pessoas.forEach(
        console.log(pratos);
     }
 )
+
+pessoas.forEach( (pessoa) => {
+    let fatia = pizza[count];
+    if( (fatia != undefined) || (fatia != null) )
+    {
+        prato.push(`${pessoa} recebeu ${fatia}`);
+        count++;
+    }
+});
+
+console.log(prato);
